4e49c04ea2dc1ce8e3551dfc9f0b1bb7d08238d9
hm535 <hm535@users.noreply.github.com>
2018-04-07 14:25:04 -0400

    function to read file

--- main.cpp
+++ main.cpp
@@ -11,24 +11,43 @@
 #include <string>
 #include "fullSolver.hpp"
 
+void readDataFile( string path , vector<double>* VGS , vector<double>* VDS , vector<double>* IDS );
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     fullSolver();
+
+    vector<double>* VGS = new vector<double>;
+    vector<double>* VDS = new vector<double>;
+    vector<double>* IDS = new vector<double>;   
     
-    string line;
-    string path = "/Users/arianabruno/Desktop/ECE4960/ProgrammingAssignments/ECE4960-PA3/outputNMOS.txt";
-//    string path = "outputNMOS.txt";
-    ifstream myfile (path);
-    if (myfile.is_open())
-    {
-        while ( getline (myfile,line) )
-        {
-            cout << line << '\n';
+    //string path = "/Users/arianabruno/Desktop/ECE4960/ProgrammingAssignments/ECE4960-PA3/outputNMOS.txt";
+    string path = "C:/Users/Haritha/Documents/ECE4960-PAs/ECE4960-PA3/outputNMOS.txt";
+    readDataFile( path , VGS ,  VDS ,  IDS );
+   
+    return 0;
+}
+
+void readDataFile( string path , vector<double>* VGS , vector<double>* VDS , vector<double>* IDS ){
+    // function to read and store file
+    ifstream myfile( path );
+    if (myfile.is_open()){
+        myfile.ignore(2048 , '\n');
+        while( !myfile.eof() ){
+            double vgs , vds , ids;
+            myfile >> vgs >> vds >> ids;
+            (*VGS).push_back(vgs);
+            (*VDS).push_back(vds);
+            (*IDS).push_back(ids);
         }
         myfile.close();
+        
+        // note that the last entry is read twice when using eof
+        // so use pop_back to get rid of extra entry
+        (*VGS).pop_back();
+        (*VDS).pop_back();
+        (*IDS).pop_back();
     }
-    
-    else cout << "Unable to open file" << endl;;
-    
-    return 0;
+    else cout << "Unable to open file " << endl;
+    return;
 }
