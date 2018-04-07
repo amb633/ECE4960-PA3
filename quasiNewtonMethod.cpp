6802cd2727dd3a76f70af0b8b89a565d9f5a2c34
amb633 <amb633@cornell.edu>
2018-04-07 18:46:23 -0400

    starting quassi newton method

---
+++ quasiNewtonMethod.cpp
@@ -0,0 +1,20 @@
+//
+//  quasiNewtonMethod.cpp
+//  PA3
+//
+//  Created by Ariana Bruno on 4/7/18.
+//  Copyright © 2018 Ariana Bruno. All rights reserved.
+//
+
+#include "quasiNewtonMethod.hpp"
+
+void quasiNetwon(vector<double>* VGS , vector<double>* VDS ){
+    vector<vector<double>*>* H = new vector<vector<double>*>;
+    zeroMatrix(H, 3);
+    
+    vector<double>* Ids;
+//    double d2V_dK2 = sum_of_squares(modelIds(), <#vector<double> *s_measured#>)
+    
+    
+    
+}
---
+++ quasiNewtonMethod.hpp
@@ -0,0 +1,21 @@
+//
+//  quasiNewtonMethod.hpp
+//  PA3
+//
+//  Created by Ariana Bruno on 4/7/18.
+//  Copyright © 2018 Ariana Bruno. All rights reserved.
+//
+
+#ifndef quasiNewtonMethod_hpp
+#define quasiNewtonMethod_hpp
+
+#include <stdio.h>
+#include <vector>
+#include "fullSolver.hpp"
+#include "utilityFunctions.cpp"
+
+using namespace std;
+
+void quasiNetwon(vector<double>* VGS , vector<double>* VDS );
+
+#endif /* quasiNewtonMethod_hpp */
